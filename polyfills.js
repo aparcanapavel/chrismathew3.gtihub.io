1941b06a4b10fe63472d32c1be4f55d274519daa
pavelaparcana <gpaparcana@gmail.com>
2019-05-06 10:59:34 -0400

    QA:Update

--- polyfills.js
+++ polyfills.js
@@ -5784,8 +5784,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/pavs/chris-app/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /Users/pavs/chris-app/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /Users/pavs/Documents/GitHub/chrisApp/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/pavs/Documents/GitHub/chrisApp/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
--- styles.js
+++ styles.js
@@ -557,7 +557,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/pavs/chris-app/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /Users/pavs/Documents/GitHub/chrisApp/src/styles.css */"./src/styles.css");
 
 
 /***/ })
